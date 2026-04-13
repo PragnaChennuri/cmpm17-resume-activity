1df5642770e8a1e3261292180516805d79058e02
Pragna Chennuri <pragna@Pragnas-MacBook-Pro-2.local>
2026-04-13 16:30:18 -0700

    feat: add examples for box model and flexbox demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-component',
@@ -9,21 +8,4 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'cmpm17-resume-activity';
-
-  currentUserName: string = 'Psi Padhya';
-
-  currentYear: number = 2026;
-
-  isCurrentYear(): boolean {
-    return this.currentYear === 2026;
-  }
-
-  handleClick(){
-    console.log("Button has been clicked");
-  }
-
-  // when I type a word on the keyboard and logs the keyboard event and the letter
-  onKeyPressEvent(event: KeyboardEvent){
-    console.log('Key Pressed', event.key);
-  }
 }
